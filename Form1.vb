115a742a0fc906acb1fcd351413e0bb2bcde9dc0
ComputerComa <68386555+ComputerComa@users.noreply.github.com>
2022-02-04 16:04:00 -0600

    Ugh-

--- Form1.vb
+++ Form1.vb
@@ -45,28 +45,42 @@ Public Class FRM_MAIN
 
     End Sub
 
-    Private Sub MS_RB_CONNECT_Click(sender As Object, e As EventArgs) Handles MS_RB_CONNECT.Click
-        Dim Validated = Form_Validate()
-        If Validated Then
-            Try
-                MS_OUTPUT.Text += "Trying to connect to " + MS_TXT_CON_IP.Text + " on port " + MS_TXT_CON_PORT.Text
-                Client.Connect(MS_TXT_CON_IP.Text, CInt(MS_TXT_CON_PORT.Text))
-                MS_OUTPUT.Text += vbNewLine + "Connection Successful!"
-            Catch ex As Exception
-                MsgBox(ex.Message)
-            End Try
-            Try
-                MS_OUTPUT.Text += vbNewLine + "Attempting to authenticate!"
-                Client.Authenticate(MS_TXT_CON_PASSWD.Text)
 
-            Catch ex As Exception
-                MS_OUTPUT.Text += vbNewLine + "Error authenticating!"
-            End Try
-        Else
-            MS_OUTPUT.Text += "Error with input data!"
+    Private Sub MS_RB_CONNECT_Click(sender As Object, e As EventArgs) Handles MS_RB_CONNECT.Click
+        If Not Client.Connected Then
+            Dim Validated = Form_Validate()
+            If Validated Then
+                Try
+                    MS_OUTPUT.Text += "Trying to connect to " + MS_TXT_CON_IP.Text + " on port " + MS_TXT_CON_PORT.Text
+                    Client.Connect(MS_TXT_CON_IP.Text, CInt(MS_TXT_CON_PORT.Text))
+                    MS_OUTPUT.Text += vbNewLine + "Connection Successful!"
+                Catch ex As Exception
+                    MsgBox(ex.Message)
+                End Try
+                Try
+                    If Client.Connected Then
+                        MS_OUTPUT.Text += vbNewLine + "Attempting to authenticate!"
+                        Client.Authenticate(MS_TXT_CON_PASSWD.Text)
+                        MS_OUTPUT.Text += vbNewLine + "Authentication Successful!"
+                    Else
+                        MS_OUTPUT.Text += vbNewLine + "Connection Aborted. Skipping authentication"
+                    End If
+
+                Catch ex As Exception
+                    MS_OUTPUT.Text += vbNewLine + "Error authenticating!"
+                    Client.Disconnect()
+
+                End Try
+            Else
+                MS_OUTPUT.Text += "Error with input data!"
+            End If
         End If
+
     End Sub
 
+    Private Sub MS_RB_QC_ADMINS_Click(sender As Object, e As EventArgs) Handles MS_RB_QC_ADMINS.Click
+        Dim Resp = Client.SendCommand("/admins")
+        MS_OUTPUT.Text += vbNewLine + Resp
 
-
+    End Sub
 End Class
